4e333d3347f48288e1277d00c98ffbebe2dbb8b7
Diego <me@diegor.nl>
2021-09-02 13:40:15 +0200

    Configure app title & icons

--- src/background.js
+++ src/background.js
@@ -19,8 +19,9 @@ async function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        title: 'Phoenix Tracker',
+        icon: 'src/assets/icon.ico',
         webPreferences: {
-
             // Use pluginOptions.nodeIntegration, leave this alone
             // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
             nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
