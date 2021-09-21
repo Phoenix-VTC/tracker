eabb6df06b78e357aa585169d261659f49c894df
Diego <me@diegor.nl>
2021-09-21 21:40:08 +0200

    Improve settings page

---
+++ src/components/SettingsSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">{{ title }}</h3>
+        <p class="mt-1 text-sm text-gray-500">
+          {{ description }}
+        </p>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <div class="space-y-6">
+          <div class="grid grid-cols-3 gap-6">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SettingsSection",
+
+  props: {
+    title: String,
+    description: String
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,85 +1,100 @@
 <template>
-  <div class="px-16 py-4">
-    <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-      <div>
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="md:col-span-1">
-            <div class="px-4 sm:px-0">
-              <h3 class="text-lg font-medium leading-6 text-gray-900">Account</h3>
-              <p class="mt-1 text-sm text-gray-600">
-                Phasellus lorem quam molestie id quisque diam aenean nulla in.
-              </p>
-            </div>
-          </div>
-          <div class="mt-5 md:mt-0 md:col-span-2">
-            <form @submit="submitAccountForm">
-              <div class="shadow sm:rounded-md">
-                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <!-- Form error message -->
-                  <div class="rounded-md bg-red-50 p-4" v-if="errors.length">
-                    <div class="flex">
-                      <div class="flex-shrink-0">
-                        <XCircleIcon class="h-5 w-5 text-red-400"/>
-                      </div>
-                      <div class="ml-3">
-                        <h3 class="text-sm font-medium text-red-800">
-                          Please check the following errors and then try again:
-                        </h3>
-                        <div class="mt-2 text-sm text-red-700">
-                          <ul class="list-disc pl-5 space-y-1">
-                            <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- End of form error message -->
-                  <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                      <label for="token" class="block text-sm font-medium text-gray-700">
-                        Tracker Token
-                      </label>
-                      <div class="mt-1">
-                        <input type="text" name="token" id="token" v-model="token"
-                               class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-md sm:text-sm border-gray-300 text-black"
-                               placeholder="Enter your token here" aria-describedby="token-description">
-                      </div>
-                      <p class="mt-2 text-sm text-gray-500" id="token-description">
-                        Keep this token save like your password, and never share it.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button type="submit"
-                          class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
-                    Save
-                  </button>
+  <div class="min-h-screen bg-gray-100">
+    <div class="bg-orange-600 pb-32">
+      <header class="py-10">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold text-white">
+            Settings
+          </h1>
+        </div>
+      </header>
+    </div>
+
+    <main class="-mt-32">
+      <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+        <div>
+          <!-- Form error message -->
+          <div class="rounded-md bg-red-50 p-4" v-if="errors.length">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <XCircleIcon class="h-5 w-5 text-red-400"/>
+              </div>
+              <div class="ml-3">
+                <h3 class="text-sm font-medium text-red-800">
+                  Please check the following errors and then try again:
+                </h3>
+                <div class="mt-2 text-sm text-red-700">
+                  <ul class="list-disc pl-5 space-y-1">
+                    <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+                  </ul>
                 </div>
               </div>
-            </form>
+            </div>
           </div>
+          <!-- End of form error message -->
+          <form class="space-y-6" @submit="submitAccountForm">
+            <settings-section title="Account" description="Update your account settings, including tracker token here.">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="token" class="block text-sm font-medium text-gray-700">Tracker token</label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <div class="relative flex items-stretch flex-grow focus-within:z-10">
+                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <key-icon class="h-5 w-5 text-gray-400"/>
+                    </div>
+                    <input :type="tokenInputType" name="token" id="token" v-model="token"
+                           class="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
+                           placeholder="John Doe" aria-describedby="token-description">
+                  </div>
+                  <button type="button" @click="toggleTokenDisplay"
+                          class="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                    <eye-icon v-if="tokenInputType === 'password'" class="h-5 w-5 text-gray-400"/>
+                    <eye-off-icon v-else class="h-5 w-5 text-gray-400"/>
+                    <span v-if="tokenInputType === 'password'">Show</span>
+                    <span v-else>Hide</span>
+                  </button>
+                </div>
+                <p class="mt-2 text-sm text-gray-500" id="token-description">
+                  Keep this token save like your password, and never share it.
+                </p>
+              </div>
+
+            </settings-section>
+
+            <div class="flex justify-end">
+              <button type="submit"
+                      class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Save
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
+    </main>
   </div>
 </template>
 
 <script>
-import { XCircleIcon } from '@heroicons/vue/solid'
+import {XCircleIcon, KeyIcon, EyeIcon, EyeOffIcon} from '@heroicons/vue/solid';
+import SettingsSection from '../components/SettingsSection';
+
 const config = require('electron-cfg');
 
 export default {
   name: "Settings",
 
   components: {
-    XCircleIcon
+    XCircleIcon,
+    KeyIcon,
+    EyeIcon,
+    EyeOffIcon,
+    SettingsSection,
   },
 
   data() {
     return {
       errors: [],
       token: config.get('tracker-token'),
+      tokenInputType: 'password'
     }
   },
 
@@ -129,6 +144,14 @@ export default {
 
           location.reload();
         })
+      }
+    },
+
+    toggleTokenDisplay() {
+      if (this.tokenInputType === 'password') {
+        this.tokenInputType = 'text';
+      } else {
+        this.tokenInputType = 'password';
       }
     }
   },
