646dbb3499a324998631c840d68ce5999ddc46b1
Diego <me@diegor.nl>
2021-10-25 17:10:49 +0200

    Remove unnecessary ternary operator from username header text [sc488]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <header class="py-10">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 class="text-3xl font-bold text-white">
-            Hey there<span v-if="this.user.username">, {{ this.user.username ?? 'Foo' }}!</span> <span v-else>!</span>
+            Hey there<span v-if="this.user.username">, {{ this.user.username }}!</span> <span v-else>!</span>
           </h1>
         </div>
       </header>
